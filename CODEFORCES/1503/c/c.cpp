6c575e6c6654bae125b28a3ac8e7a97bb66775c3
LuminaDCIX <aster2632@gmail.com>
2021-05-11 19:51:15 +0800

    0511

--- CODEFORCES/1503/c/c.cpp
+++ CODEFORCES/1503/c/c.cpp
@@ -3,24 +3,26 @@
 using namespace std;
 
 #define y1 _y1
-#define all(x) x.begin(), x.end()
-
+#define all(x) (x).begin(), (x).end()
+#define gid(x, id) (get<(id)>((x)))
 
 
 int main() {
   ios::sync_with_stdio(false);
   cin.tie(0);
-  vector<pair<int, int> > v;
   int n;
   cin >> n;
+  vector<pair<int, int> > v(n);
+  long long ans = 0;
   for (int i = 0; i < n; ++i) {
     cin >> v[i].first >> v[i].second;
+    ans += gid(v[i], 1);
   }
-  sort(v.begin() + 1, v.end());
-  v.push_back(v[0]);
-  long long ans = 0;
-  for (int i = 1; i <= n; ++i) {
-    ans += max(v[i].second, v[i].first - v[i - 1].first);
+  sort(v.begin(), v.end());
+  int mx = gid(v[0], 0) + gid(v[0], 1);
+  for (int i = 1; i < n; ++i) {
+    ans += max(0, gid(v[i], 0) - mx);
+    mx = max(mx, gid(v[i], 0) + gid(v[i], 1));
   }
   cout << ans << endl;
   return 0;
